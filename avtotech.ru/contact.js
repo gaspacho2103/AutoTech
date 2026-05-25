c8aab4ba0f8d55e0f923a18692c654810084bb5a
Waldemar Tkatskiy <130499580+gaspacho2103@users.noreply.github.com>
2026-05-25 23:19:10 +0400

    Update contact.js

--- avtotech.ru/contact.js
+++ avtotech.ru/contact.js
@@ -1,38 +1,67 @@
+// Функция экранирования (защита от XSS)
+function escapeHtml(str) {
+    if (!str) return '';
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
 async function getContacts() {
-    let res = await fetch('http://api.avtotech.ru/contacts');
-    let contacts = await res.json();
+    const telephoneDiv = document.querySelector('.telephone');
+    const footerContainer = document.querySelector('.footer__container');
 
-    // document.querySelector('.table').innerHTML = '';
+    if (!telephoneDiv || !footerContainer) return;
 
-    contacts.forEach((contact) => {
-        document.querySelector('.telephone').innerHTML += `
+    try {
+        const res = await fetch('http://api.avtotech.ru/contacts');
         
-            
-           <img src="assets/icons/russia.png" alt="" class="ru">
-            <a class="phone" href="">${contact.telephone}</a>
-        
-        
-        `
+        if (!res.ok) {
+            throw new Error(`HTTP ${res.status}: ${res.statusText}`);
+        }
 
-        document.querySelector('.footer__container').innerHTML += `
-        
-            
-           <div class="contacts">
-				<h2 class="footer__title">Наши контакты:</h2>
-				<ul>
-					<li><a href="#!"><img src="assets/icons/telega.svg"> ${contact.telegram}</a></li>
-					<li><a href="#!"><img src="assets/icons/telephone.svg"> ${contact.telephone}</a></li>
-					<li><a href="#!"><img src="assets/icons/mail.svg"> ${contact.email}</a></li>
-				</ul>
-			</div>
-			<div class="adress">
-				<h2 class="footer__title">Наш адрес:</h2>
-				<h3 class="adress__desc">${contact.address}</h3>
-			</div>
-        
-        
-        `
-    });
+        const contacts = await res.json();
+        if (!contacts || contacts.length === 0) return;
+
+        // Очищаем перед вставкой (чтобы не дублировать при повторных вызовах)
+        telephoneDiv.innerHTML = '';
+        footerContainer.innerHTML = '';
+
+        contacts.forEach((contact) => {
+            const safeTelephone = escapeHtml(contact.telephone);
+            const safeTelegram = escapeHtml(contact.telegram);
+            const safeEmail = escapeHtml(contact.email);
+            const safeAddress = escapeHtml(contact.address);
+
+            // Вставляем в .telephone
+            telephoneDiv.innerHTML += `
+                <img src="assets/icons/russia.png" alt="" class="ru">
+                <a class="phone" href="tel:${safeTelephone}">${safeTelephone}</a>
+            `;
+
+            // Вставляем в .footer__container
+            footerContainer.innerHTML += `
+                <div class="contacts">
+                    <h2 class="footer__title">Наши контакты:</h2>
+                    <ul>
+                        <li><a href="#!"><img src="assets/icons/telega.svg"> ${safeTelegram}</a></li>
+                        <li><a href="#!"><img src="assets/icons/telephone.svg"> ${safeTelephone}</a></li>
+                        <li><a href="#!"><img src="assets/icons/mail.svg"> ${safeEmail}</a></li>
+                    </ul>
+                </div>
+                <div class="adress">
+                    <h2 class="footer__title">Наш адрес:</h2>
+                    <h3 class="adress__desc">${safeAddress}</h3>
+                </div>
+            `;
+        });
+    } catch (error) {
+        console.error('Ошибка загрузки контактов:', error);
+        // Можно показать заглушку, но не ломать страницу
+        if (telephoneDiv) telephoneDiv.innerHTML = '<span class="error">Контакты временно недоступны</span>';
+    }
 }
 
 getContacts();
